933ea27c15a8af5ffc1fcc8f4cb43dc2acde0c0c
Nathan Novak <nnovak@ku.edu>
2018-08-03 09:50:19 -0700

    change to handlebars, css,  and js files

--- controllers/burger_controllers.js
+++ controllers/burger_controllers.js
@@ -1,17 +1,48 @@
 var express = require("express");
-var burgers = require("../models/burger.js");
+var burger = require("../models/burger.js");
 
 var router = express.Router();
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burgers.selectAll(function(data) {
+  burger.selectAll(function(data) {
+    console.log("DATA!!!", data);
     var hbsObject = {
       burgers: data
     };
-    console.log(hbsObject);
+    // console.log(hbsObject);
     res.render("index", hbsObject);
   });
+});
+
+router.post("/api/burgers", function(req, res) {
+  console.log("POSTED");
+  console.log("REQ:", req.body.burger_name);
+  burger.insertOne(["burger_name"], [req.body.burger_name], function(result) {
+
+	  res.json({ id: result.insertId });
+  	console.log("POSTED:", result);
+  // res.send(result);
+  });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.updateOne({ devoured: req.body.devoured }, condition, function(result) {
+
+      console.log("Devoured in UPDATEONE", req.body.devoured);
+      console.log(result);
+      if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
+
+    }
+  );
 });
 
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,12 +1,25 @@
 var orm = require("../config/orm.js");
 
-var burgers = {
+var burger = {
 	selectAll: function(cb){
 		orm.selectAll("burgers", function(res){
 			cb(res);
-		})
+		});
 	}, 
 
-}
+	insertOne: function(col, val, cb){
+		console.log(col, val);
+		orm.insertOne("burgers", col, val, function(res){
+			console.log("InsertOne:", res);
+			cb(res);
+		});
+	},
 
-module.exports = burgers;
+	updateOne: function(ob, condition, cb){
+		orm.updateOne("burgers", ob, condition, function(res){
+			cb(res);
+		});
+	}
+};
+
+module.exports = burger;
